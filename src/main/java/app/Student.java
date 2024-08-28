01585becfda2aa44d1a524277dbedf3e249f40eb
Jenjolen <cph-jn466@cphbusiness.dk>
2024-08-28 11:20:18 +0200

    created Student class and branch

---
+++ src/main/java/app/Student.java
@@ -0,0 +1,24 @@
+package app;
+
+import java.time.LocalDate;
+
+public class Student {
+
+    String name;
+
+    String phonenumber;
+
+    String email;
+
+    String address;
+
+    Boolean isStudying;
+
+    LocalDate dateOfBirth;
+
+    LocalDate enrollmentDate;
+
+
+
+
+}
