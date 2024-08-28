9529f75c907791a2a9e64bff43460247a591e692
Loudi <loudimail@gmail.com>
2024-08-28 11:38:00 +0200

    CourseDAO finished

--- src/main/java/app/CourseDAO.java
+++ src/main/java/app/CourseDAO.java
@@ -1,23 +1,60 @@
 package app;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
 public class CourseDAO implements GenericDAO<Course, Integer> {
+
+    private static CourseDAO instance;
+    private static EntityManagerFactory emf;
+
+    public static CourseDAO getInstance(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new CourseDAO();
+        }
+        return instance;
+    }
+
     @Override
     public void saveEntity(Course entity) {
-
+        try(EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.persist(entity);
+            em.getTransaction().commit();
+        }
     }
 
     @Override
     public int deleteEntity(Integer id) {
-        return 0;
+        try(EntityManager em = emf.createEntityManager()) {
+            Course deletedCourse = em.find(Course.class, id);
+            em.getTransaction().begin();
+            em.remove(deletedCourse);
+            em.getTransaction().commit();
+            return deletedCourse.getId();
+        }
     }
 
     @Override
     public Course findEntity(Integer id) {
-        return null;
+        try(EntityManager em = emf.createEntityManager()) {
+                return em.find(Course.class, id);
+        }
     }
 
     @Override
     public Course updateEntity(Course entity, Integer id) {
-        return null;
+        try(EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            Course updatedCourse = em.merge(entity);
+            em.getTransaction().commit();
+            return updatedCourse;
+        }
+    }
+
+    public void close(){
+        emf.close();
     }
 }
